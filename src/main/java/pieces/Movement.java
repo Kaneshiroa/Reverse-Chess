75f4e40fc8f22d258196f738e69d9b3306836c01
Jeffrey <meijeffrey73@gmail.com>
2026-03-03 18:34:18 -0500

    Added base packages with skeletons for pieces/boards, need to build logic upon the board

---
+++ src/main/java/pieces/Movement.java
@@ -0,0 +1,14 @@
+package pieces;
+
+public interface Movement {
+
+    /* Define different movements for each piece type
+       King moves x1 in diag/horizontal/vertical
+       Queen moves all possible squares diag/horizontal/vertical
+       Bishop moves all possible diag squares
+       Rook moves all vertical/horizontal squares
+       Knight moves all L shaped squares, ex. 2 up 1 right, 2 up 1 left, 2 right 1 down, 2 left 1 down
+       Pawn moves x1 forward besides start, remember en passant, captures top right or top left
+     */
+
+}
---
+++ src/main/java/pieces/Pieces.java
@@ -0,0 +1,34 @@
+package pieces;
+
+public class Pieces implements Movement{
+
+    public static abstract class King {
+
+
+    }
+
+    public static abstract class Queen {
+
+
+    }
+
+    public static abstract class Knight {
+
+
+    }
+
+    public static abstract class Bishop {
+
+
+    }
+
+    public static abstract class Rook {
+
+
+    }
+
+    public static abstract class Pawn {
+
+
+    }
+}
