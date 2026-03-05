bebcebea0fee92b9e3c5dddf45291065179f21a7
Jeffrey <meijeffrey73@gmail.com>
2026-03-05 16:14:54 -0500

    Removed redundant methods to reduce clutter, added methods to check if tile is filled or empty, and added check for isEnemy

--- src/main/java/board/Board.java
+++ src/main/java/board/Board.java
@@ -1,6 +1,7 @@
 package board;
 
 import datastructures.Vector2D;
+import pieces.King;
 import pieces.Piece;
 
 public class Board {
@@ -36,7 +37,20 @@ public class Board {
         piece.setLoc(loc);
     }
 
+    //Check if a piece is inside specified tile
     public boolean isInside(Vector2D loc) {
         return loc.getX() >= 0 && loc.getX() < 8 && loc.getY() >= 0 && loc.getY() < 8;
+    }
+
+    //Method to check if a tile is empty
+    public boolean isEmpty(Vector2D loc) {
+        return getPiece(loc) == null;
+    }
+
+    public boolean isEnemy(Vector2D loc, String color) {
+        //Get our piece
+        Piece piece = getPiece(loc);
+        //Check its color, if it isn't what we enter as the parameter, that means its an enemy
+        return piece != null && !piece.getColor().equals(color);
     }
 }
--- src/main/java/pieces/Rook.java
+++ src/main/java/pieces/Rook.java
@@ -7,38 +7,13 @@ import java.util.List;
 
 public class Rook extends Piece implements Movement{
 
-    private String color;
-    private Vector2D location;
-
     public Rook(String color, Vector2D location) {
         super(location, color);
     }
 
     @Override
-    public Vector2D getLoc() {
-        return super.getLoc();
-    }
-
-    @Override
-    public String getColor() {
-        return color;
-    }
-
-    @Override
     public boolean isValidMove(Vector2D loc, Board board) {
-        return loc.getX() <= 8 && loc.getX() >= 0 && loc.getY() <= 8 && loc.getY() >= 0;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    //TODO: Get logic working for isValidMove to work with the conditional to set a location
-    public void setLocation(Vector2D location) {
-        if (location.getX() < 0 || location.getY() < 0 || location.getY() >= 8 || location.getX() >= 8) {
-            throw new IllegalArgumentException("Cannot place piece outside board");
-        }
-        this.location = location;
+        return possibleMoves(board).contains(loc);
     }
 
     @Override
