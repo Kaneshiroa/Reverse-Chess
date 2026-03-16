69428f87066a4fcc957d6dddc650771304493688
Kaneshiroa <meijeffrey73@gmail.com>
2026-03-16 01:34:51 -0400

    Added a method to check all backrank squares to ensure a King cannot castle through squares in scope of enemies

--- src/main/java/board/Board.java
+++ src/main/java/board/Board.java
@@ -1,8 +1,7 @@
 package board;
 
 import datastructures.Vector2D;
-import pieces.King;
-import pieces.Piece;
+import pieces.*;
 
 public class Board {
 
@@ -34,7 +33,9 @@ public class Board {
             throw new IllegalArgumentException("Cannot place piece outside board");
         }
         board[loc.getX()][loc.getY()] = piece;
-        piece.setLoc(loc);
+        if (piece != null) {
+            piece.setLoc(loc);
+        }
     }
 
     public void Move(Vector2D start, Vector2D end) {
@@ -64,5 +65,101 @@ public class Board {
         Piece piece = getPiece(loc);
         //Check its color, if it isn't what we enter as the parameter, that means its an enemy
         return piece != null && !piece.getColor().equals(color);
+    }
+
+    public boolean isSquareAttacked(Vector2D target, String myColor) {
+        String enemyColor;
+        if (myColor.equals("White")) {
+            enemyColor = "Black";
+        } else {
+            enemyColor = "White";
+        }
+        //Sliding Attackers (Rook, Bishop, Queen)
+        //Horizontal/Vertical (Looking for Rook or Queen)
+        int[][] rookDirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        if (checkSliding(target, enemyColor, rookDirs, true)) {
+            return true;
+        }
+        //Diagonals (Looking for Bishop or Queen)
+        int[][] bishopDirs = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+        if (checkSliding(target, enemyColor, bishopDirs, false)) {
+            return true;
+        }
+        //Knights
+        int[][] knightMoves = {{2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}};
+        for (int[] m : knightMoves) {
+            Vector2D p = new Vector2D(target.getX() + m[0], target.getY() + m[1]);
+            if (isInside(p)) {
+                Piece piece = getPiece(p);
+                if (piece != null) {
+                    if (piece instanceof Knight && piece.getColor().equals(enemyColor)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        //Pawns
+        int pawnDirection;
+        if (myColor.equals("White")) {
+            pawnDirection = 1; // Look up for Black pawns
+        } else {
+            pawnDirection = -1; // Look down for White pawns
+        }
+
+        int[][] pawnCapture = {{1, pawnDirection}, {-1, pawnDirection}};
+        for (int[] c : pawnCapture) {
+            Vector2D p = new Vector2D(target.getX() + c[0], target.getY() + c[1]);
+            if (isInside(p)) {
+                Piece piece = getPiece(p);
+                if (piece != null) {
+                    if (piece instanceof Pawn && piece.getColor().equals(enemyColor)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        //Kings
+        int[][] kingDirs = {{1,0}, {-1,0}, {0,1}, {0,-1}, {1,1}, {1,-1}, {-1,1}, {-1,-1}};
+        for (int[] k : kingDirs) {
+            Vector2D p = new Vector2D(target.getX() + k[0], target.getY() + k[1]);
+            if (isInside(p)) {
+                Piece piece = getPiece(p);
+                if (piece != null) {
+                    if (piece instanceof King && piece.getColor().equals(enemyColor)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    //Helper function for isSquareAttacked
+    private boolean checkSliding(Vector2D target, String enemyColor, int[][] dirs, boolean isRookStyle) {
+        for (int[] d : dirs) {
+            int x = target.getX() + d[0];
+            int y = target.getY() + d[1];
+
+            while (x >= 0 && x < 8 && y >= 0 && y < 8) {
+                Piece p = board[x][y];
+                if (p != null) {
+                    if (p.getColor().equals(enemyColor)) {
+                        if (p instanceof Queen) {
+                            return true;
+                        }
+                        if (isRookStyle && p instanceof Rook) {
+                            return true;
+                        }
+                        if (!isRookStyle && p instanceof Bishop) {
+                            return true;
+                        }
+                    }
+                    break; //Path blocked by any piece
+                }
+                x += d[0];
+                y += d[1];
+            }
+        }
+        return false;
     }
 }
